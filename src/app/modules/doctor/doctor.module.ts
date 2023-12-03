cfe08bd65f03cc9ebd55384330f52f32327c9c8d
subashshakya <subash.shakya@bajratechnologies.com>
2023-12-03 14:48:43 +0545

    feat: add doctor routing

--- src/app/modules/doctor/doctor.module.ts
+++ src/app/modules/doctor/doctor.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FormsModule } from '@angular/forms';
+import { DoctorRoutingModule } from './doctor.routing';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { CalendarComponent } from './shared/calendar/calendar.component';
+import { PatientReportListComponent } from './shared/patient-report-list/patient-report-list.component';
+import { ReportListItemComponent } from './shared/report-list-item/report-list-item.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    NavBarComponent,
+    CalendarComponent,
+    PatientReportListComponent,
+    ReportListItemComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    DoctorRoutingModule,
+    InputTextModule
   ]
 })
 export class DoctorModule { }
