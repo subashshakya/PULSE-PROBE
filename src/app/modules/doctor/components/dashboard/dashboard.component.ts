1fa6fad6aff8471ea08ac8a262d36a33948d6e06
subashshakya <subash.shakya@bajratechnologies.com>
2023-12-14 19:35:45 +0545

    feat: add dashboard implementation

--- src/app/modules/doctor/components/dashboard/dashboard.component.ts
+++ src/app/modules/doctor/components/dashboard/dashboard.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DoctorService } from '../../services/doctor.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DoctorReport } from '../../models/doctor.model';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  public doctorData: any;
+  public reports: any;
+  public docId: string | null = '';
+  public doctorReportsById: DoctorReport[] = [];
 
+  constructor(
+    private docService: DoctorService, 
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getPatientInfo();
+  }
+  
+  public tableHeadings = ['Field', 'Normal', 'Actual'];
+  public getPatientInfo(): void {
+    this.docService.getReportsByDoctorId(2).subscribe(res => {
+      this.doctorReportsById = res;
+      console.log(this.doctorReportsById);
+      console.log(this.doctorReportsById[0].patientId);
+    })
+  }
+
+  public goToDetailsPage(patientId: number, reportId: number) {
+    this.router.navigate(['/doctor/report-detail/', patientId], { queryParams: { reportId: reportId }});
+  }
 }
--- src/app/modules/doctor/doctor.routing.ts
+++ src/app/modules/doctor/doctor.routing.ts
@@ -1,11 +1,25 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { ReportDetailsComponent } from "./components/report-details/report-details.component";
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent
+  },
+  {
+    path: 'doctor',
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'report-detail/:id',
+        component: ReportDetailsComponent
+      },
+    ]
   }
 ];
 
